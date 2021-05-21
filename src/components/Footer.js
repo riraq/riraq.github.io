267a4cd476feb4d6dd0c5357940cf59bba2cd687
riraq <riraq@mail.usf.edu>
2021-05-21 18:20:52 -0500

    changed class to className in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ function Footer() {
 
 
   return (
-    <footer class="footer row">
-    <div class="row footerContainer">
+    <footer className="footer row">
+    <div className="row footerContainer">
       <h3>Contact</h3>
       <address>
         Name: Rony Iraq<br/>
         Email: <a href="mailto:riraq25@gmail.com">riraq25@gmail.com</a><br/>
         Phone: (561) 685-0578<br/>
-        <a href="https://github.com/riraq" class="gx-4 px-2 col">GitHub</a>
-        <a href="https://www.linkedin.com/in/rony-iraq/" class="gx-4 px-2 col">LinkedIn</a>
-        <a href="./assets/rony-iraq-resume.pdf" class="gx-4 px-2 col">Resume</a>
+        <a href="https://github.com/riraq" className="gx-4 px-2 col">GitHub</a>
+        <a href="https://www.linkedin.com/in/rony-iraq/" className="gx-4 px-2 col">LinkedIn</a>
+        <a href="./assets/rony-iraq-resume.pdf" className="gx-4 px-2 col">Resume</a>
       </address>
     </div>
   </footer>
