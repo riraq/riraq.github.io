ae8638e27a0b8b19ef2b57d8aabc5e9cd5c05015
riraq <riraq@mail.usf.edu>
2021-05-22 17:19:40 -0500

    fixed navbar brand route

--- src/components/NavbarBrand.js
+++ src/components/NavbarBrand.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavbarBrand() {
   return (
-      <a className="navbar-brand" href="index.html">Rony Iraq</a>
+      <a className="navbar-brand" href="/">Rony Iraq</a>
   )
 }
 
